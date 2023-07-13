190829ea2524684576aa64ac3f0349501ea0a6c0
kaancnkya <kaancnkya@gmail.com>
2023-07-14 02:02:57 +0300

    API_KEY, BASE_URL, IMDB_ID constants have created in constants file.

---
+++ app/src/main/java/com/example/movieland/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.movieland.util
+
+object Constants {
+    const val API_KEY = "b55d0c32"
+    const val BASE_URL = "https://www.omdbapi.com/"
+    const val IMDB_ID = "imdb_id"
+
+}
