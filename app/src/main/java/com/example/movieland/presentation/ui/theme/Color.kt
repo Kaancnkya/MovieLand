a27a6702e551864eec0f0eb7892a7ae3ef2db942
kaancnkya <kaancnkya@gmail.com>
2023-07-14 01:42:06 +0300

    I have created the clean architecture structure to start the project.

--- app/src/main/java/com/example/movieland/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/movieland/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.movieland.ui.theme
+package com.example.movieland.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/movieland/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movieland/presentation/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.movieland.ui.theme
+package com.example.movieland.presentation.ui.theme
 
 import android.app.Activity
 import android.os.Build
